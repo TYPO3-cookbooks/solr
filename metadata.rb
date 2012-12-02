ec98749020cc1f081513b4d499c18e1fc5d4b665
Steffen Gebert <steffen.gebert@typo3.org>
2012-12-02 21:43:34 +0100

    Add cookbooks tomcat and solr

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "TYPO3 Association"
+maintainer_email "steffen.gebert@typo3.org"
+license          "MIT"
+description      "Installs/Configures solr"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "tomcat"
