6bb7e3b62486a3a749b069e16e488ef6fe496d58
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-03 00:35:20 +0100

    Update version to 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "MIT"
 description      "Installs/Configures solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 
 depends "tomcat"
 depends "ark"
