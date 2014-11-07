44d09dfe564cef11e9b7ff83e2c2a4952cd97ab5
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-07 21:46:38 +0100

    Bump solr

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "MIT"
 description      "Installs/Configures solr"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.9"
+version          "0.2.10"
 
 depends "tomcat", "~> 0.12.0"
 depends "ark", "~> 0.4.0"
